4611193e59391af3803f35fad6a00a2f67c0e239
Johans Neira <johansneirap@gmail.com>
2022-09-12 22:43:19 -0300

    chore: first commit

---
+++ test/App.test.jsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import App from '../src/App';
+
+describe('App Component', () => {
+  it('should render the component initially', () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+});
